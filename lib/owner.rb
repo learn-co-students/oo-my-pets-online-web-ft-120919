9d4678f13070a5456c200c062686d6b3f6deae93
github username <github email address>
2020-01-08 18:52:40 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,10 +42,35 @@ class Owner
   end
 
   def buy_cat(name)
-    # Make a new instance of the appropriate pet, initializing it with 
-    # that name and the owner who is purchasing it.
-    binding.pry
-    Cat.new
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+       cat.mood = "nervous"   
+       cat.owner = nil
+    end
+    
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil 
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 end
