62855de52ef680b8a1f4625478f33d4bddb83f73
Devon James Wright <devonjamess.wright@gmail.com>
2020-01-08 22:54:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+  @@all = []
+  attr_accessor :owner, :mood
+  attr_reader :name, :species
+  def initialize (name,owner)
+   @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @species = "dog"
+    @@all << self
+  end 
+  def self.all
+    @@all
+  end 
 end
