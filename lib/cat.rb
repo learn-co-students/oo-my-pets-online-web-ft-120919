e12777ceda9311e67f41b3070dcbb19de2792762
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-01-09 13:38:32 -0800

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  attr_writer
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  def self.all #class method
+    @@all
+  end
+
+  def save #class method
+    @@all << self
+  end
+
+
+
 end
